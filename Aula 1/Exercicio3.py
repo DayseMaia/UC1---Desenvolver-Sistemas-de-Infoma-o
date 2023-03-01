ea2ab474161c36df7a25afe6dd4e2b325604d7fc
DayseMaia <126699781+DayseMaia@users.noreply.github.com>
2023-03-01 13:58:29 -0300

    Add files via upload

---
+++ Aula 1/Exercicio3.py
@@ -0,0 +1,17 @@
+item1 = input("Digite o item 1: ")
+item2 = input("Digite o item 2: ")
+item3 = input("Digite o item 3: ")
+item4 = input("Digite o item 4: ")
+item5 = input("Digite o item 5: ")
+
+qtde1 = input("Digite a quantidade do item 1: ")
+qtde2 = input("Digite a quantidade do item 2: ")
+qtde3 = input("Digite a quantidade do item 3: ")
+qtde4 = input("Digite a quantidade do item 4: ")
+qtde5 = input("Digite a quantidade do item 5: ")
+
+print(f"A quantidade do {item1} é {qtde1}")
+print(f"A quantidade do {item2} é {qtde2}")
+print(f"A quantidade do {item3} é {qtde3}")
+print(f"A quantidade do {item4} é {qtde4}")
+print(f"A quantidade do {item5} é {qtde5}")
---
+++ Aula 1/Exercicio4.py
@@ -0,0 +1,17 @@
+Fruta1 = input("Digite o nome da Fruta 1: ")
+Fruta2 = input("Digite o nome da Fruta 2: ")
+Fruta3 = input("Digite o nome da Fruta 3: ")
+Fruta4 = input("Digite o nome da Fruta 4: ")
+Fruta5 = input("Digite o nome da Fruta 5: ")
+
+Fruta1Preco = 1
+Fruta2Preco = Fruta1Preco*2
+Fruta3Preco = Fruta1Preco/2
+Fruta4Preco = Fruta3Preco*3
+Fruta5Preco = Fruta4Preco/2
+
+print(f"A fruta {Fruta1} custa R$ {Fruta1Preco}")
+print(f"A fruta {Fruta2} custa R$ {Fruta2Preco}")
+print(f"A fruta {Fruta3} custa R$ {Fruta3Preco}")
+print(f"A fruta {Fruta4} custa R$ {Fruta4Preco}")
+print(f"A fruta {Fruta5} custa R$ {Fruta5Preco}")
