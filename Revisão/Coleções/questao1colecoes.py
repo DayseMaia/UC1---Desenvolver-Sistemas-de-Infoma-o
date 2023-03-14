37e0d30e4cff32d9e25e685ccc4a33f4a5ee0835
DayseMaia <126699781+DayseMaia@users.noreply.github.com>
2023-03-14 19:13:59 -0300

    Update questao1colecoes.py

--- Revisão/Coleções/questao1colecoes.py
+++ Revisão/Coleções/questao1colecoes.py
@@ -1,5 +1,13 @@
 #1. Escreva um programa que leia uma lista de números inteiros e imprima o maior e o menor número da lista.
 
-lista = [10, 20, 30, 40, 50]
+lista_numeros = []
+qtd_numeros = int(input("Quantos números deseja na lista? "))
 
-print("Maior número:", max(lista), "\nMenor número:", min(lista))
+for i in range(qtd_numeros):
+    numero = int(input("Digite um número inteiro: "))
+    lista_numeros.append(numero)
+    
+maior = max(lista_numeros)
+menor = min(lista_numeros)
+
+print("O maior número é:", maior, "\nO menor número é:", menor)
