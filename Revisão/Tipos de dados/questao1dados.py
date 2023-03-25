8d44d95391fe5ff4021406955a5c5f5f180c61f7
DayseMaia <126699781+DayseMaia@users.noreply.github.com>
2023-03-25 18:46:59 -0300

    Update questao1dados.py

--- Revisão/Tipos de dados/questao1dados.py
+++ Revisão/Tipos de dados/questao1dados.py
@@ -1,7 +1,10 @@
 #1. Escreva um programa que solicite ao usuário um número inteiro e imprima o dobro desse número.
 
+#Solicitando ao usuário um número:
 numero = float(input("Digite um número: "))
 
+#Calculando o dobro:
 dobro = numero * 2
 
+#Imprimindo o resultado:
 print(f"O dobro de {numero} é igual a: {dobro}.")
