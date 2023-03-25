a353d45c6bc85cbb4ab5ad25cd9539f25f970761
DayseMaia <126699781+DayseMaia@users.noreply.github.com>
2023-03-25 17:31:30 -0300

    Update questao3ef.py

--- Revisão/Escopo de Funções/questao3ef.py
+++ Revisão/Escopo de Funções/questao3ef.py
@@ -1,4 +1,3 @@
-#Escreva uma função que receba uma lista de números inteiros como parâmetro e retorne a média dos números. 
-# A função deve verificar se a lista está vazia e retornar None caso esteja. 
-# Em seguida, utilize uma variável global para contar quantas vezes a função foi chamada 
-# e imprimir o valor da contagem ao final de cada chamada.
+'''Escreva uma função que receba uma lista de números inteiros como parâmetro e retorne a média dos números. 
+A função deve verificar se a lista está vazia e retornar None caso esteja. 
+Em seguida, utilize uma variável global para contar quantas vezes a função foi chamada e imprimir o valor da contagem ao final de cada chamada.'''
