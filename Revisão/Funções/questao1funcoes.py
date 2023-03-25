2553903fe261a8857f9890faa515c8c3f2030a62
DayseMaia <126699781+DayseMaia@users.noreply.github.com>
2023-03-25 18:13:56 -0300

    Update questao1funcoes.py

--- Revisão/Funções/questao1funcoes.py
+++ Revisão/Funções/questao1funcoes.py
@@ -1,8 +1,12 @@
 #1. Escreva uma função que receba uma lista de números inteiros e retorne o maior número da lista.
 
+#Função que retorna o maior número:
 def maior_numero(numeros):
     numeros = max(numeros)
     return numeros
 
+#Criando uma lista:
 lista_numeros = [10, 20, 30, 40, 50]
+
+#Chamando a função e imprimindo o resultado:
 print("Maior número:", maior_numero(lista_numeros))
