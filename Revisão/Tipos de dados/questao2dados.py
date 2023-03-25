e235f74a731b5b8cdd9c3314c0f3326f3785039e
DayseMaia <126699781+DayseMaia@users.noreply.github.com>
2023-03-25 18:49:22 -0300

    Update questao2dados.py

--- Revisão/Tipos de dados/questao2dados.py
+++ Revisão/Tipos de dados/questao2dados.py
@@ -1,7 +1,10 @@
 #2. Escreva um programa que solicite ao usuário o raio de um círculo e calcule a área desse círculo.
 
+#Solicitando ao usuário o raio do círculo:
 raio = float(input("Digite o raio do círculo: "))
 
+#Calculando a área do círculo:
 area = 3.142 * (raio**2)
 
+#Imprimindo o resultado:
 print("A área do círculo é:", area)
