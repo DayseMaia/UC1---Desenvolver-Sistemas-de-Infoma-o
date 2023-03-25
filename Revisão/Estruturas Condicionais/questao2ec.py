394584da5997a384ca3a220d38a46163a1f4f8e5
DayseMaia <126699781+DayseMaia@users.noreply.github.com>
2023-03-25 17:46:08 -0300

    Update questao2ec.py

--- Revisão/Estruturas Condicionais/questao2ec.py
+++ Revisão/Estruturas Condicionais/questao2ec.py
@@ -1,11 +1,12 @@
-#2. Escreva um programa que leia o nome e a idade de uma pessoa e imprima uma mensagem personalizada com base na idade. 
-# Se a idade for menor que 18 anos, imprima "Você é menor de idade". 
-# Se a idade estiver entre 18 e 65 anos, imprima "Você é adulto". 
-# Caso contrário, imprima "Você é idoso".
+'''2. Escreva um programa que leia o nome e a idade de uma pessoa e imprima uma mensagem personalizada com base na idade. 
+Se a idade for menor que 18 anos, imprima "Você é menor de idade". Se a idade estiver entre 18 e 65 anos, imprima "Você é adulto". 
+Caso contrário, imprima "Você é idoso".'''
 
+#Solicitando nome e idade ao usuário:
 nome = input("Digite seu nome: ")
 idade = input("Digite sua idade: ")
 
+#Verificando as condições solicitadas e imprimindo o resultado:
 if nome and idade:
     idade = int(idade)
     if idade < 18:
@@ -16,3 +17,4 @@ if nome and idade:
         print("Você é idoso.")
 else:
     print("Valor inválido!")
+    
