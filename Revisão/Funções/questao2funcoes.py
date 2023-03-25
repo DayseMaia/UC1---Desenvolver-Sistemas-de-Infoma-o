0d42c044a7ebce4e40d8b2f54dd655a2da60d5e7
DayseMaia <126699781+DayseMaia@users.noreply.github.com>
2023-03-25 18:32:10 -0300

    Update questao2funcoes.py

--- Revisão/Funções/questao2funcoes.py
+++ Revisão/Funções/questao2funcoes.py
@@ -1,2 +1,15 @@
 #Escreva uma função que receba uma lista de palavras e retorne uma lista contendo apenas as palavras que começam com a letra "a"
 
+#Funão que recebe uma lista e retorna as palavras que começam com "a":
+def letra_a(lista):
+    lista_a = []
+    for i in lista:
+        if i.startswith("a"):
+            lista_a.append(i)
+    return lista_a
+
+#Criando lista:
+lista = ["aceitar", "aprender", "bola", "rede", "alegria"]
+
+#Chamando a lista e imprimindo resultado:
+print(letra_a(lista))
