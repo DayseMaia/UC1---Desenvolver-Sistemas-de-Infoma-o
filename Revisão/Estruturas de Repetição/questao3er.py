a7456ff384edc29a2c75c2d7b43a3cb2c6aca64a
DayseMaia <126699781+DayseMaia@users.noreply.github.com>
2023-03-25 18:00:04 -0300

    Update questao3er.py

--- Revisão/Estruturas de Repetição/questao3er.py
+++ Revisão/Estruturas de Repetição/questao3er.py
@@ -1,6 +1,8 @@
 #3. Escreva um programa que solicite ao usuário um número e imprima a tabuada desse número até o valor 10.
 
+#Solicitando ao usuário um número:
 tabuada = int(input("Digite um número: "))
 
+#Gerando uma lista de 1 a 10 para calcular com o valor digitado pelo usuário e imprimindo os resultados:
 for i in range(1,11):
     print(f"{tabuada} X {i} = {tabuada*i}")
