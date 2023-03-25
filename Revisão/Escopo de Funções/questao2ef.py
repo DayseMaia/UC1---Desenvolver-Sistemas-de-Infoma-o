da07d1f270accf5f830e23026ee4d93c369fabaf
DayseMaia <126699781+DayseMaia@users.noreply.github.com>
2023-03-25 17:28:39 -0300

    Update questao2ef.py

--- Revisão/Escopo de Funções/questao2ef.py
+++ Revisão/Escopo de Funções/questao2ef.py
@@ -1,14 +1,22 @@
-#2. Escreva um programa que solicite ao usuário um número e imprima se esse número é par ou ímpar. 
-#Crie uma função para determinar se um número é par e outra função para determinar se um número é ímpar.
+'''2. Escreva um programa que solicite ao usuário um número e imprima se esse número é par ou ímpar. 
+Crie uma função para determinar se um número é par e outra função para determinar se um número é ímpar.'''
 
-def numero_par(num):
-    if num % 2 == 0:
-        print("É par")
+#Função que verifica se o número é par:
+def num_par(numero):
+    return numero % 2 == 0
 
-def numero_impar(num):
-    if num % 2 != 0:
-        print("É ímpar")
+#Função que verifica se o número é ímpar:
+def num_impar(numero):
+    return numero % 2 == 1
 
-numero = float(input("Digite um número: "))
+#Solicitando número ao usuário:
+numero = int(input("Digite um número: "))
 
-print
+#Verificando e imprimindo se o número é par ou ímpar:
+if num_par(numero):
+    print(f"O número {numero} é par.")
+elif num_impar(numero):
+    print(f"O número {numero} é ímpar.")
+else:
+    print("Número inválido!")
+    
