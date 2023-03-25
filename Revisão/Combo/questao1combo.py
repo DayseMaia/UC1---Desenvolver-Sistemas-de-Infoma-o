7e27c92c9d953f40a62e1168d1791e9231bb6e30
DayseMaia <126699781+DayseMaia@users.noreply.github.com>
2023-03-25 14:55:57 -0300

    Update questao1combo.py

--- Revisão/Combo/questao1combo.py
+++ Revisão/Combo/questao1combo.py
@@ -1,3 +1,22 @@
-#Escreva uma função que receba como parâmetro uma lista de números inteiros e retorne a soma dos números pares na lista. 
-# Em seguida, utilize um laço de repetição para solicitar ao usuário uma lista de números inteiros, 
-# chame a função e imprima o resultado.
+'''Escreva uma função que receba como parâmetro uma lista de números inteiros e retorne a soma dos números pares na lista. 
+Em seguida, utilize um laço de repetição para solicitar ao usuário uma lista de números inteiros, chame a função e imprima o resultado.'''
+
+#Função que retorna a soma dos números pares na lista:
+def somar_pares(lista):
+    soma = 0
+    for num in lista:
+        if num % 2 == 0:
+            soma += num
+    return soma
+
+lista = []
+
+#Solicitando ao usuário os números inteiros:
+while True:
+    numero = input("Digite um número inteiro (ou 'sair' para terminar): ")
+    if numero == "sair" or numero == "Sair":
+        break
+    lista.append(int(numero))
+    
+#Imprimindo o resultado com a função:
+print(f"A soma dos números pares é: {somar_pares(lista)}")
