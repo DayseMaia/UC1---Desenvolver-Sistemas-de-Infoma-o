d406ca85f888bdf4a559218c20b6f23e0ca0e2c8
DayseMaia <126699781+DayseMaia@users.noreply.github.com>
2023-03-25 16:20:41 -0300

    Update questao2colecoes.py

--- Revisão/Coleções/questao2colecoes.py
+++ Revisão/Coleções/questao2colecoes.py
@@ -1,8 +1,9 @@
-#2. Escreva um programa que leia uma lista de nomes e crie um dicionário 
-#onde a chave é o nome e o valor é o número de vezes que o nome aparece na lista.
+'''2. Escreva um programa que leia uma lista de nomes e crie um dicionário onde a chave é o nome e o valor é o número de vezes que o nome aparece na lista.'''
 
+#Criando uma lista:
 lista_nomes = ["A", "aranha", "arranha", "a", "rã", "A", "rã", "arranha", "a", "aranha", "Nem", "a", "aranha", "arranha", "a", "rã", "Nem", "a", "rã", "arranha", "a", "aranha",]
 
+#Criando um dicionário:
 contagem_nomes = {}
 for nome in lista_nomes:
     if nome in contagem_nomes:
@@ -10,4 +11,5 @@ for nome in lista_nomes:
     else:
         contagem_nomes[nome] = 1
         
+#Imprimindo resultado:
 print(contagem_nomes)
