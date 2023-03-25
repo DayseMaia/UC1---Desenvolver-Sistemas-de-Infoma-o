b2a210029c76ea2c422b4242d3f7353765d1e99e
DayseMaia <126699781+DayseMaia@users.noreply.github.com>
2023-03-25 17:49:03 -0300

    Update questao3ec.py

--- Revisão/Estruturas Condicionais/questao3ec.py
+++ Revisão/Estruturas Condicionais/questao3ec.py
@@ -1,10 +1,12 @@
-#3. Escreva um programa que solicite ao usuário a nota de um aluno em uma prova e imprima a mensagem 
-# "Aprovado" se a nota for maior ou igual a 7, 
-# "Reprovado" se a nota for menor que 5 e 
-# "Recuperação" se a nota estiver entre 5 e 7.
+'''3. Escreva um programa que solicite ao usuário a nota de um aluno em uma prova e imprima a mensagem 
+"Aprovado" se a nota for maior ou igual a 7, 
+"Reprovado" se a nota for menor que 5 e 
+"Recuperação" se a nota estiver entre 5 e 7.'''
 
+#Solicitando ao usuário a nota:
 nota = input("Insira sua nota: ")
 
+#Verificando as condições solicitadas e imprimindo os resultados:
 if nota:
     nota = float(nota)
     if nota >= 7:
