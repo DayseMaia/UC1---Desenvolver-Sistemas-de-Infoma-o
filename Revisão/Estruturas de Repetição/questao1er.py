7dea119ce212fc127afd331e495395a572be7ea8
DayseMaia <126699781+DayseMaia@users.noreply.github.com>
2023-03-25 17:54:22 -0300

    Update questao1er.py

--- Revisão/Estruturas de Repetição/questao1er.py
+++ Revisão/Estruturas de Repetição/questao1er.py
@@ -1,4 +1,5 @@
 #1. Escreva um programa que imprima todos os números ímpares entre 1 e 50.
 
+#Utilizando o loop for com o range para listar os ímpares entre 1 e 50 e imprimindo o resultado:
 for i in range(1,51,2):
     print(i)
