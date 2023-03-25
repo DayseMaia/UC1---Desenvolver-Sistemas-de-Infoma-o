c98b61dbf1fe94a1c98c957902679a31a72d4469
DayseMaia <126699781+DayseMaia@users.noreply.github.com>
2023-03-25 17:56:28 -0300

    Update questao2er.py

--- Revisão/Estruturas de Repetição/questao2er.py
+++ Revisão/Estruturas de Repetição/questao2er.py
@@ -1,8 +1,11 @@
 #2. Escreva um programa que leia uma lista de números inteiros e imprima a média desses números.
 
+#Criando uma lista:
 lista = [10, 20, 30, 40, 50]
 
+#Calculando média:
 media = sum(lista) / len(lista)
 
+#Imprimindo resultados:
 print(lista)
 print("A média da lista é:", media)
