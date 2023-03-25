832996dd23fb914e9b5a8d913f5f7d7df423efc3
DayseMaia <126699781+DayseMaia@users.noreply.github.com>
2023-03-25 18:45:03 -0300

    Update questao4funcoes.py

--- Revisão/Funções/questao4funcoes.py
+++ Revisão/Funções/questao4funcoes.py
@@ -1,2 +1,2 @@
-#4. Escreva uma função que receba uma lista de dicionários contendo informações sobre pessoas 
-# (nome, idade, cidade) e retorne uma lista contendo apenas os nomes das pessoas que moram em uma cidade específica.
+'''4. Escreva uma função que receba uma lista de dicionários contendo informações sobre pessoas (nome, idade, cidade) 
+e retorne uma lista contendo apenas os nomes das pessoas que moram em uma cidade específica.'''
