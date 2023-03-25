7a38b528802a970f500f20c9241679a267389baa
DayseMaia <126699781+DayseMaia@users.noreply.github.com>
2023-03-25 18:50:32 -0300

    Update questao3dados.py

--- Revisão/Tipos de dados/questao3dados.py
+++ Revisão/Tipos de dados/questao3dados.py
@@ -1,6 +1,8 @@
 #3. Escreva um programa que solicite ao usuário seu nome e idade e imprima uma mensagem personalizada com essas informações.
 
+#Solicitando ao usuário nome e idade:
 nome = input("Digite seu nome: ")
 idade = input("Digite sua idade: ")
 
+#Imprimindo o resultado:
 print(f"Seu nome é {nome} e sua idade é {idade}.")
